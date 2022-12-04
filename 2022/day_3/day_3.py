5d0a383c533f8f95c5d41f1c925569cdbb63e8f3
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2022-12-04 16:22:09 +0100

    Added day 3 2022

---
+++ 2022/day_3/day_3.py
@@ -0,0 +1,10 @@
+file_input = open("2022/day_3/day_3_input.txt").read().splitlines()
+
+value = lambda x: ord(x) -(38  if x.isupper() else 96)
+
+conv1= lambda x: value(*set(x[:len(x)//2]) & ( set(x[len(x)//2:])))
+print(sum(map(conv1, file_input)))
+
+
+conv2 = lambda x: value(*(set(file_input[x]) & set(file_input[x+1]) & set(file_input[x+2])))
+print(sum(map(conv2, range(0,  len(file_input), 3))))
