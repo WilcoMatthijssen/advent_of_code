fe441dd927a8f7286c594ab2c7d736f2bf24ffac
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2022-12-06 19:23:56 +0100

    Added day 6 2022

---
+++ 2022/day_6/day_6.py
@@ -0,0 +1,15 @@
+input_file = open("2022/day_6/day_6_input.txt").read()
+
+# for i in range(len(input_file)):
+#     if len(set(input_file[i-4:i])) == 4:
+#         print(i)
+#         break
+
+# for i in range(len(input_file)):
+#     if len(set(input_file[i-14:i])) == 14:
+#         print(i)
+#         break
+
+
+print("part1:", next(i for i in range(len(input_file)) if len(set(input_file[i-4:i])) == 4))
+print("part2:", next(i for i in range(len(input_file)) if len(set(input_file[i-14:i])) == 14))
