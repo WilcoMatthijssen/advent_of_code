17478c66185dd754e35579fc8c2a0ab2ca420e07
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2023-12-02 22:14:18 +0100

    Move day 6 2022

--- 2022/code/day_6.py
+++ 2022/code/day_6.py
@@ -1,4 +1,4 @@
-input_file = open("2022/day_6/day_6_input.txt").read()
+input_file = open("2022/input/day_6.txt").read()
 
 # for i in range(len(input_file)):
 #     if len(set(input_file[i-4:i])) == 4:
