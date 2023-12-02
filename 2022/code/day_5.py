b4dec77e5d8a231f60aabb68c984fcdfb8c06f0c
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2023-12-02 22:14:04 +0100

    Move day 5 2022

--- 2022/code/day_5.py
+++ 2022/code/day_5.py
@@ -1,4 +1,4 @@
-input_file = open("2022/day_5/day_5_input.txt").readlines()
+input_file = open("2022/input/day_5.txt").readlines()
 
 rules = map(lambda string: eval(string.replace("move", "").replace("from", ",").replace("to", ",")), input_file[10:])
 crates1 = [""] + [*map(lambda row: "".join(row).replace(" ", "")[::-1], [*zip(*input_file[:8])][1::4])]
