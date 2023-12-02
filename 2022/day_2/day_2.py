3863411c5942820ca03858ccd825175bbb7bf2a8
Wilco <57558724+WilcoMatthijssen@users.noreply.github.com>
2023-12-02 22:11:41 +0100

    Move day 2 2022

--- 2022/day_2/day_2.py
+++
@@ -1,2 +0,0 @@
-print("first", sum(map("   B XC YA ZA XB YC ZC XA YB Z".index, open("2022/day_2/day_2_input.txt").read().splitlines())) // 3)
-print("second",sum(map("   B XC XA XA YB YC YC ZA ZB Z".index, open("2022/day_2/day_2_input.txt").read().splitlines())) // 3)
